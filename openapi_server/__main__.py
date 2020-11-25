aa1c43c7c0ab8697047ebe6c2a260d7ee326baaa
JoserraLP <jolozanop@alumnos.unex.es>
2020-11-25 12:03:37 +0100

    Added port to Flask app

--- openapi_server/__main__.py
+++ openapi_server/__main__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import connexion
+import os
 
 from flask_sqlalchemy import SQLAlchemy
 
@@ -10,6 +11,7 @@ db = SQLAlchemy()
 
 def main():
 
+    port = int(os.environ.get('PORT', 5000))
     app = connexion.App(__name__, specification_dir='./openapi/')
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('openapi.yaml', arguments={'title': 'Historic Management API'})
@@ -20,7 +22,7 @@ def main():
     with app.app.app_context():
         db.create_all()
 
-    app.run(port=8080)
+    app.run(host='0.0.0.0', port=port)
 
 
 if __name__ == '__main__':
