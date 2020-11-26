e374f35bf8e85624cbc5effa55d988be450bb5a9
JoserraLP <jolozanop@alumnos.unex.es>
2020-11-26 19:50:55 +0100

    Procfile updated

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn openapi_server.__main__:main -b 0.0.0.0:$PORT --log-file -
+web: gunicorn openapi_server.__main__:app -b 0.0.0.0:$PORT
--- openapi_server/__main__.py
+++ openapi_server/__main__.py
@@ -5,14 +5,17 @@ from openapi_server.config.default import *
 
 from openapi_server import encoder
 
-port = int(os.environ.get('PORT', 8080))
+port = int(os.environ.get('PORT', 8080)) 
+
 
 def main():
-    app = connexion.App(__name__, specification_dir='./openapi/')  
+    app = connexion.App(__name__, specification_dir='./openapi/') 
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('openapi.yaml', arguments={'title': 'Historic Management API'})
     app.app.config.from_object(DevelopmentConfig)
     app.run(host='0.0.0.0', port=port)
+
+app = main()
 
 if __name__ == '__main__':
     app = main()
