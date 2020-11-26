c02605c0f6dae22eaeab6cdab827520c12dcf25d
JoserraLP <jolozanop@alumnos.unex.es>
2020-11-26 23:25:34 +0100

    Procfile updated

--- openapi_server/__main__.py
+++ openapi_server/__main__.py
@@ -7,11 +7,12 @@ from openapi_server import encoder
 
 
 def main():
+    port = int(os.environ.get("PORT", 5000))
     app = connexion.App(__name__, specification_dir='./openapi/') 
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('openapi.yaml', arguments={'title': 'Historic Management API'})
     app.app.config.from_object(DevelopmentConfig)
-    app.run()
+    app.run(host='0.0.0.0', port=port)
 
 app = main()
 
