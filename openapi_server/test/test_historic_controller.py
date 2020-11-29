13be57abce5ee04aa45bac694ea17709d407db01
SergioFdezRc <s.fernandez.rincon@gmail.com>
2020-11-29 17:56:53 +0100

    Se han mockeado los tests y se ha modificado el fichero travis.yml

--- openapi_server/test/test_historic_controller.py
+++ openapi_server/test/test_historic_controller.py
@@ -2,6 +2,8 @@
 
 from __future__ import absolute_import
 
+from unittest import mock
+
 from flask import json
 from six import BytesIO
 
@@ -12,43 +14,81 @@ from openapi_server.test import BaseTestCase
 class TestHistoricController(BaseTestCase):
     """HistoricController integration test stubs"""
 
-    def test_add_visit(self):
+    @mock.patch("muii_g2_family_lock_database.Database.PostgresDB.add_visit")
+    def test_add_visit(self, mocked_add_visit):
         """Test case for add_visit
 
         Add a visit to historic
         """
         visit = Visit()
+        mocked_add_visit.assert_not_called()
         response = self.client.open(
             '/historic',
             method='POST',
             data=json.dumps(visit),
             content_type='application/json')
+        mocked_add_visit.assert_called_once()
         self.assert200(response,
                        'Response body is : ' + response.data.decode('utf-8'))
 
-    def test_get_all_historic(self):
+    @mock.patch("muii_g2_family_lock_database.Database.PostgresDB.get_all_historic")
+    def test_get_all_historic(self, mocked_get_all_historic):
         """Test case for get_all_historic
 
         Get all visits from historic
         """
+        mocked_get_all_historic.assert_not_called()
+        mocked_get_all_historic.return_value = [
+            [
+                "27/11/2020",
+                1,
+                "ff:ff:ff:ff:ff:ff",
+                "2:07"
+            ],
+            [
+                "27/11/2020",
+                2,
+                "00:00:00:00:00",
+                "2:07"
+            ],
+            [
+                "27/11/2020",
+                3,
+                "00:00:00:00:00",
+                "2:07"
+            ]
+        ]
         response = self.client.open(
             '/historic',
             method='GET')
+        mocked_get_all_historic.assert_called_once()
         self.assert200(response,
                        'Response body is : ' + response.data.decode('utf-8'))
 
-    def test_get_visit(self):
+    @mock.patch("muii_g2_family_lock_database.Database.PostgresDB.get_visit")
+    def test_get_visit(self, mocked_get_visit):
         """Test case for get_visit
 
         Get one visit's information
         """
+        mocked_get_visit.assert_not_called()
+        mocked_get_visit.return_value = [
+            [
+                "27/11/2020",
+                1,
+                "ff:ff:ff:ff:ff:ff",
+                "2:07"
+            ],
+        ]
         response = self.client.open(
             '/historic/{id}'.format(id=3),
             method='GET')
+        mocked_get_visit.assert_called_once()
         self.assert200(response,
                        'Response body is : ' + response.data.decode('utf-8'))
 
 
 if __name__ == '__main__':
     import unittest
+
     unittest.main()
